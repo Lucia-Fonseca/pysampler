8b199941f57f57fa45330c426654690fe8002ef0
Lucia F. de la Bella <55983939+Lucia-Fonseca@users.noreply.github.com>
2021-06-24 09:07:04 +0100

    Create __init__.py

---
+++ pysampler/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package contains functions to sample from any probability distribution and
+provides statistics from sampling many realisations of such distributions.
+"""
+__version__ = '0.1'
